bf12ce11622fc737a3a76a271206f1d9f9feee06
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-06 12:20:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
